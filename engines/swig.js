a16d9528bbb3c10101555da61f3dd9d9e2a825e1
limingv5 <limingv5@gmail.com>
2016-04-07 13:59:46 +0800

    swig

---
+++ engines/swig.js
@@ -0,0 +1,21 @@
+module.exports = function (htmljsfile, reqOpt, param, cb) {
+  var swig = require("swig");
+  var helper = require("../lib/util");
+
+  var htmlfile = htmljsfile.replace(/(\.swig)\.js$/, "$1");
+  var tpl = helper.getUnicode(htmlfile);
+  if (tpl !== null) {
+    var result = swig.precompile(tpl).tpl.toString().replace("anonymous", '');
+
+    cb(false, (param.compatible ? "return " : "module.exports=") + result);
+  }
+  else {
+    tpl = helper.getUnicode(htmljsfile);
+    if (tpl !== null) {
+      cb(false, tpl);
+    }
+    else {
+      cb({code: "Not Found"});
+    }
+  }
+};
