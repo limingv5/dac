5987fafcbb76d731700ffc7551437858d801e65f
limingv5 <limingv5@gmail.com>
2017-02-07 11:08:47 +0800

    Push commit by liming.mlm

--- engines/lesspolymer.js
+++ engines/lesspolymer.js
@@ -2,18 +2,23 @@ module.exports = function (absPath, filteredUrl, reqOpt, param, cb) {
   var lessLayer = require("./less");
   var helper = require("../lib/util");
 
-  absPath = absPath.replace(/\.html$/, '');
+  if (param.ignore && helper.matchPath(filteredUrl, param.ignore)) {
+    cb({code: -1});
+  }
+  else {
+    absPath = absPath.replace(/\.html$/, '');
 
-  lessLayer(absPath, filteredUrl, reqOpt, param, function (err, compiled) {
-    if (err) {
-      cb(err);
-    }
-    else {
-      compiled = '<dom-module id="' + helper.filteredUrl(filteredUrl, param.filter) + '">' +
-        '<template><style>' +
-        compiled +
-        '</style></template></dom-module>';
-      cb(null, compiled);
-    }
-  });
+    lessLayer(absPath, filteredUrl, reqOpt, param, function (err, compiled) {
+      if (err) {
+        cb(err);
+      }
+      else {
+        compiled = '<dom-module id="' + helper.filteredUrl(filteredUrl, param.filter) + '">' +
+          '<template><style>' +
+          compiled +
+          '</style></template></dom-module>';
+        cb(null, compiled);
+      }
+    });
+  }
 };
